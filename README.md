8711e5fd5620903477e2e3dd164fe575763ca755
estevam02009 <manoel.neto53861@alunos.ufersa.edu.br>
2022-05-02 15:06:10 -0300

    Add componentes e páginas especifica

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Projeto Comercio local
 
 - O objetivo do projeto é ligar os comerciantes locais a clientes.
+
+# Cor utilizada
+#60b0f4
---
+++ src/Routes/Routes.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Routes, Route, Router } from 'react-router-dom'
+
+export default function Routes() {
+  return (
+    <div>Routes</div>
+  )
+}
